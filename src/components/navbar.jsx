5fe818858f02ab8143daafb21d8dd65a384b49e6
anigmea <divyanshkanodia673@gmail.com>
2025-01-16 16:33:21 -0800

    fix

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import {React,useEffect,useState} from "react";
 import styled from "styled-components";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCartShopping, faUser } from "@fortawesome/free-solid-svg-icons";
