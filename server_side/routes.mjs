dc97224420186b581547617fd91ad45bdd0e2f0e
anigmea <divyanshkanodia673@gmail.com>
2025-01-16 17:36:54 -0800

    fix

--- server_side/routes.mjs
+++ server_side/routes.mjs
@@ -355,8 +355,8 @@ app.post("/login", async (req, res) => {
         return res.status(404).json({ message: "User not found" });
       }
 
-      const user = results[0];
-      const token = jwt.sign(user.UserId , JWT_SECRET, { expiresIn: '1d' });
+      // const user = results[0];
+      // const token = jwt.sign(user.UserId , JWT_SECRET, { expiresIn: '1d' });
 
       // // Check if the provided userID matches the user's ID in the database
       // if (user.UserID !== userID) {
@@ -379,7 +379,7 @@ app.post("/login", async (req, res) => {
           email: user.Email,
           firstName: user.FirstName,
           lastName: user.LastName,
-          token: token
+          // token: token
         },
       });
     });
