a6178bea4aa9fc45fdc594bfb56e66189cdb55a8
anigmea <divyanshkanodia673@gmail.com>
2025-01-16 22:51:43 -0800

    fix

--- server_side/routes.mjs
+++ server_side/routes.mjs
@@ -235,10 +235,11 @@ app.delete("/api/cart_items/:cart_id", async (req, res) => {
 
 app.delete("/api/cart_items", async (req, res) => {
   const { user_id } = req.query;
-  return res.status(200).json({ message: user_id });
+  
   
 
   if (!user_id || isNaN(user_id)) {
+    return res.status(200).json({ message: user_id });
     return res.status(400).json({ message: "Invalid UserID" });
   }
 
